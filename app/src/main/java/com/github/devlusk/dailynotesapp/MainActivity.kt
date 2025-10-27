5f1df45b37378416147a5f0ebd5195ffc0352b45
Lucas <lucasvida2005@gmail.com>
2025-10-27 14:30:05 -0300

    feat: add intent to switch Activity and adjusts margins in layouts

--- app/src/main/java/com/github/devlusk/dailynotesapp/MainActivity.kt
+++ app/src/main/java/com/github/devlusk/dailynotesapp/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.github.devlusk.dailynotesapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.devlusk.dailynotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnAddNote.setOnClickListener {
+            val intent = Intent(this, CreateNoteActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
