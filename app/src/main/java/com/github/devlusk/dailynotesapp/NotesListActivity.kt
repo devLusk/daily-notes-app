d1aa314227b8864e904086c77eb5ad34bb7179b9
Lucas <lucasvida2005@gmail.com>
2025-10-28 14:40:46 -0300

    feat: add card for note item

--- app/src/main/java/com/github/devlusk/dailynotesapp/NotesListActivity.kt
+++ app/src/main/java/com/github/devlusk/dailynotesapp/NotesListActivity.kt
@@ -1,20 +1,35 @@
 package com.github.devlusk.dailynotesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.devlusk.dailynotesapp.databinding.ActivityNotesListBinding
+import com.github.devlusk.dailynotesapp.databinding.ItemNoteBinding
+import java.io.File
 
 class NotesListActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNotesListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_notes_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityNotesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
