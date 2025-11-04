be1d7fcb7d6dba6aef2a3a471e1ce0ad06c6145c
Lucas <lucasvida2005@gmail.com>
2025-11-04 12:22:37 -0300

    feat: add Note data class

---
+++ app/src/main/java/com/github/devlusk/dailynotesapp/Note.kt
@@ -0,0 +1,7 @@
+package com.github.devlusk.dailynotesapp
+
+data class Note(
+    val title: String,
+    val content: String,
+    val filePath: String
+)
--- app/src/main/java/com/github/devlusk/dailynotesapp/NotesListActivity.kt
+++ app/src/main/java/com/github/devlusk/dailynotesapp/NotesListActivity.kt
@@ -1,18 +1,11 @@
 package com.github.devlusk.dailynotesapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.github.devlusk.dailynotesapp.databinding.ActivityNotesListBinding
-import com.github.devlusk.dailynotesapp.databinding.ItemNoteBinding
-import java.io.File
 
 class NotesListActivity : AppCompatActivity() {
     lateinit var binding: ActivityNotesListBinding
